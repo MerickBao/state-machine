e8c84e57047a58ae507117faf0a38669f12d7e85
MerickBao <1247602405@qq.com>
2022-08-08 16:02:13 +0800

    init

---
+++ src/main/java/com/example/demo/api/DemoApi.java
@@ -0,0 +1,19 @@
+package com.example.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: merickbao
+ * @Created_Time: 2022-08-08 15:42
+ * @Description: 测试服务是否正常启动
+ */
+
+@RestController
+public class DemoApi {
+
+	@GetMapping("/test-query")
+	public String testQuery() {
+		return "hello world!";
+	}
+}
